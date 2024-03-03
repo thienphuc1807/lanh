37f3d10b965b51d4667a4294d81af0eb004e09d1
thienphuc1807 <90585795+thienphuc1807@users.noreply.github.com>
2024-03-03 16:00:16 +0700

    fix is not a module

--- src/lib/serveraction.ts
+++ src/lib/serveraction.ts
@@ -11,38 +11,3 @@ export const handleGithubLogout = async () => {
     await signOut();
 }
 
-// import { revalidatePath } from "next/cache"
-// import { Products } from "./models"
-// import { connectToDb } from "./utils"
-
-// interface FormData {
-//     name?: string | "";
-//     price?: string | "";
-//     salePrice?: string | "";
-//     ingredient?: string | "";
-// }
-
-
-// export const addProduct = async (formData: FormData) => {
-//     console.log(formData);
-
-//     // Destructuring
-//     const { name, price, salePrice, ingredient } = Object.fromEntries(formData);
-//     try {
-
-//         // Connect to the database
-//         await connectToDb();
-
-//         // Create a new product
-//         const newProduct = new Products({ name, price, ingredient, salePrice });
-
-//         // Save the new product
-//         await newProduct.save();
-
-//         // Revalidate the "/products" path
-//         revalidatePath("/products");
-//     } catch (error) {
-//         console.error("Something went wrong:", error);
-//     }
-// };
-
